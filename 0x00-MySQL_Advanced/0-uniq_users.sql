c5edce503a02b1703e84e28a12c23a1010ff21e2
Glenn444 <gmark1586@gmail.com>
2024-10-16 09:08:47 +0300

    0. We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Create Users Table
+-- With unique constraints
+CREATE TABLE IF NOT EXISTS users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
